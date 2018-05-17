de0d0737d8fbe039b4caa7019096b05b2dacbd1c
Suphaloet Vongkunkij <suphaloet.skp@gmail.com>
2018-05-17 08:39:19 +0700

    add bootstrap4

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { FAB } from 'react-material-design';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<FAB 
-    location="floating-bottom-right"
-    icon="create_new_folder"
-  />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
