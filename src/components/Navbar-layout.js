5bc9b822d59f8e19e93938457dd8564abe7a9991
Suphaloet Vongkunkij <suphaloet.skp@gmail.com>
2018-05-20 18:27:31 +0700

    add icon and install chart js

--- src/components/Navbar-layout.js
+++ src/components/Navbar-layout.js
@@ -30,33 +30,24 @@ class NavbarLayout extends Component {
         return (
             <div>
                 <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/">reactstrap</NavbarBrand>
+                    <NavbarBrand href="/">
+                        City Soundscape
+                        </NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
                             <NavItem>
-                                <NavLink href="/components/">Components</NavLink>
+                                <NavLink href="/heatmap3d/">Heatmap 3D</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                                <NavLink href="/heatmap2d/">Heatmap 2D</NavLink>
                             </NavItem>
-                            <UncontrolledDropdown nav inNavbar>
-                                <DropdownToggle nav caret>
-                                    Options
-                </DropdownToggle>
-                                <DropdownMenu right>
-                                    <DropdownItem>
-                                        Option 1
-                  </DropdownItem>
-                                    <DropdownItem>
-                                        Option 2
-                  </DropdownItem>
-                                    <DropdownItem divider />
-                                    <DropdownItem>
-                                        Reset
-                  </DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
+                            <NavItem>
+                                <NavLink href="/graph/">Graph</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/calculate/">Calculate</NavLink>
+                            </NavItem>
                         </Nav>
                     </Collapse>
                 </Navbar>
