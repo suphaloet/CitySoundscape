1c2886ced95fc10dea2de9bbccf08c94cf797b7a
Suphaloet Vongkunkij <suphaloet.skp@gmail.com>
2018-05-21 23:15:40 +0700

    Router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import NavbarLayout from './components/Navbar-layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter as Router, Redirect } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+import Heatmap3D from './pages/heatmap-3d';
 class App extends Component {
   render() {
     return (
-      <NavbarLayout />
-
+      <div>
+        <NavbarLayout />
+        <Router>
+        <div className="App">
+          <Route path="/" render={()=>{return(<Redirect to="/heatmap3d/"/>);}}/>
+          <Route path="/heatmap3d/" exact strict component={ Heatmap3D }/>
+        </div>
+      </Router>
+      </div>
       
     );
   }
---
+++ src/pages/heatmap-3d.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Heatmap3D extends Component {
+    render(){
+        return (
+            <div>Test
+            </div>
+        );
+    }
+}
+
+export default Heatmap3D;
